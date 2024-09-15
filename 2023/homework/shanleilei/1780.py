ce85a7fa3afe3a8023362863fa623ab916dbeb25
Restyled.io <commits@restyled.io>
2024-09-15 05:18:15 +0000

    Restyled by autopep8

--- 2023/homework/shanleilei/1780.py
+++ 2023/homework/shanleilei/1780.py
@@ -3,6 +3,6 @@ class Solution:
         conut = sum(
             1 for i in range(len(s)) if s[i] == '1' and (i == 0 or s[i - 1] == '0')
         )
-        #判断计数是否大于1
-        #大于1则意味着存在不连续结构即01或10
+        # 判断计数是否大于1
+        # 大于1则意味着存在不连续结构即01或10
         return conut <= 1
